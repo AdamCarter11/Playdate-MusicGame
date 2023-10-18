603643144045a92dcde3d2ed3044d2b00e394e0b
Adam <57573881+AdamCarter11@users.noreply.github.com>
2023-10-17 21:53:06 -0700

    added player shake + more libraries

--- VSCode-PlaydateTemplate-master/source/main.lua
+++ VSCode-PlaydateTemplate-master/source/main.lua
@@ -1,17 +1,43 @@
+import "CoreLibs/object"
 import "CoreLibs/graphics"
+import "CoreLibs/sprites"
+import "CoreLibs/timer"
 
 local pd <const> = playdate
 local gfx <const> = pd.graphics
 
 local playerX, playerY = 200, 120
+local originalPlayerX, originalPlayerY = playerX, playerY
 local playerRadius = 10
 local playerSpeed = 3
 
+local screenWidth = 400
+local screenHeight = 240
+local screenShakeIntensity = 5
 
 function  pd.update()
     gfx.clear()
     local crankAngle = math.rad(pd.getCrankPosition())
-    playerX += math.sin(crankAngle) * playerSpeed
-    playerY -= math.cos(crankAngle) * playerSpeed
+    local newX = playerX + math.sin(crankAngle) * playerSpeed
+    local newY = playerY - math.cos(crankAngle) * playerSpeed
+
+    -- Check if the new position is out of the screen bounds
+    if newX - playerRadius < 0 or newX + playerRadius > screenWidth or newY - playerRadius < 0 or newY + playerRadius > screenHeight then
+        -- Perform screen shake
+        performScreenShake()
+    else
+        playerX, playerY = newX, newY
+        originalPlayerX, originalPlayerY = playerX, playerY
+    end
+
+    gfx.fillCircleAtPoint(playerX, playerY, playerRadius)
+end
+
+function performScreenShake()
+    local xOffset = (math.random() * 2 - 1) * screenShakeIntensity
+    local yOffset = (math.random() * 2 - 1) * screenShakeIntensity
+    playerX = originalPlayerX + xOffset
+    playerY = originalPlayerY + yOffset
+
     gfx.fillCircleAtPoint(playerX, playerY, playerRadius)
 end
