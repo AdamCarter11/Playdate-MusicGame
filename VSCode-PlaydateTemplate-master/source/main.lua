73130d540f7d5d6c25f38b699d52f7baff8aaa8d
Adam <57573881+AdamCarter11@users.noreply.github.com>
2023-10-22 12:42:21 -0700

    uploaded temp sprites + bunch of stuff
    - started creating code structure (will eventually need to split up scripts
    - go sprite movement + checking position in
    - figured out timers
    - expiremented with sprite setup functions that unfortunately won't work for what we need to do

--- VSCode-PlaydateTemplate-master/source/main.lua
+++ VSCode-PlaydateTemplate-master/source/main.lua
@@ -6,11 +6,13 @@ import "CoreLibs/timer"
 local pd <const> = playdate
 local gfx <const> = pd.graphics
 
+-- anything below this comment can be rewritten/changed/deleted
 local playerX, playerY = 200, 120
 local originalPlayerX, originalPlayerY = playerX, playerY
 local playerRadius = 10
 local playerSpeed = 3
 
+-- screen dimensions for the playdate are 400x240
 local screenWidth = 400
 local screenHeight = 240
 local screenShakeIntensity = 5
@@ -20,9 +22,74 @@ local filePlayer= pd.sound.fileplayer.new("Sounds/TheWorstPolyphia", 5)
 filePlayer:play(3)
 filePlayer:setVolume(1.0)
 
+-- an array of key inputs keyed to matching strings so we can use it to determine sprite spawn
+local keyInputs = {
+    playdate.kButtonUp,
+    playdate.kButtonRight,
+    playdate.kButtonDown,
+    playdate.kButtonLeft
+}
+-- timer has to be created above the update cause update has to recall this
+local timerInterval = 3 * 600
+function SpawnArrow()
+    print("spawn arrow")
+end
+local spawnTimer = pd.timer.new(timerInterval, SpawnArrow)
+spawnTimer.repeats = true
+spawnTimer:start()
+
+-- define image vars globally so the sprite setup function can access them
+local testImge
+local arrowLeftSprite
+
+-- main game logic functions
+-- start is called at the very end to make sure all necessary functions have been made
+function Start()
+    -- testImge = gfx.image.new("Sprites/TestPlaydateSprite")
+    -- assert(testImge) -- make sure image was loaded correctly
+    -- SpriteSetup(testImge, 100, 100, 2)
+
+    local arrowLeft = gfx.image.new("Sprites/LeftArrow")
+    assert(arrowLeft)
+    arrowLeftSprite = gfx.sprite.new(arrowLeft)
+    arrowLeftSprite:moveTo(400,120)
+    arrowLeftSprite:add()
+    
+end
 
 function  pd.update()
+    -- clear the screen before redrawing
     gfx.clear()
+
+    -- just testing stuff with this function
+    -- CheckMoveShake()
+
+    arrowLeftSprite:moveBy(-2,0)
+    --[[
+    local randoDir = keyInputs[math.random(1, #keyInputs)]
+    if pd.buttonIsPressed(randoDir) then
+        print("pressed correct button")
+    end
+    ]]
+    if pd.buttonIsPressed(pd.kButtonLeft) and arrowLeftSprite.x < 150 then
+        print("correct button pressed")
+    end
+
+   
+    -- draw the needed sprites
+    drawSprites()
+
+    -- update timers
+    pd.timer.updateTimers()
+end
+
+function drawSprites()
+    gfx.sprite.update()
+    -- gfx.fillCircleAtPoint(playerX, playerY, playerRadius)
+end
+
+-- extra functions
+function CheckMoveShake()
     local crankAngle = math.rad(pd.getCrankPosition())
     local newX = playerX + math.sin(crankAngle) * playerSpeed
     local newY = playerY - math.cos(crankAngle) * playerSpeed
@@ -35,8 +102,6 @@ function  pd.update()
         playerX, playerY = newX, newY
         originalPlayerX, originalPlayerY = playerX, playerY
     end
-
-    gfx.fillCircleAtPoint(playerX, playerY, playerRadius)
 end
 
 function performScreenShake()
@@ -47,3 +112,27 @@ function performScreenShake()
 
     gfx.fillCircleAtPoint(playerX, playerY, playerRadius)
 end
+
+
+-- unfortunately, we need a sprite var as it acts as the obj var, so this function won't work
+    -- a function I created, it defaults sprite dim to 32x32, but you can type a 5 parameter in for the dimension
+    -- ex: SpriteSetup(testImge, posX, posY, scaleMulti, 64) 64 is the sprite dimensions
+    function SpriteSetup(image, posX, posY, scaleMulti, spriteDim)
+        if spriteDim == nil then
+           spriteDim = 32 
+        end
+        tempSprite = gfx.sprite.new(image)
+        tempSprite:moveTo(posX,posY)
+        local tempScale = 3
+        -- really janky, but the scale is what actually sets the size
+        tempSprite:setScale(scaleMulti,scaleMulti)
+        -- set size sets the max area the sprite can take
+        -- 32x32 is this sprites size, then we multi it by scale to increase its actual size in the playdate
+        tempSprite:setSize(32*scaleMulti,32*scaleMulti)
+        tempSprite:add()
+    end
+
+
+
+-- call start
+Start()
