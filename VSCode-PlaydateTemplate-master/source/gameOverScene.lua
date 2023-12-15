eab06380e4092e74da69e2cbc9baa2ddc3e8a014
Sawyer <sporte@ucsc.edu>
2023-12-14 16:24:16 -0800

    game over scene!
    muah

--- VSCode-PlaydateTemplate-master/source/gameOverScene.lua
+++ VSCode-PlaydateTemplate-master/source/gameOverScene.lua
@@ -9,14 +9,41 @@ class('GameOverScene').extends(gfx.sprite)
 
 function GameOverScene:init(text)
     resetHealth()
-    local gameOverImage = gfx.image.new(gfx.getTextSize(text))
+    local backgroundImage = gfx.image.new("Sprites/GameOver")
+    local borderImage = gfx.image.new("Sprites/EvenBiggerBoarder")
+    local borderImageButBigger = gfx.image.new("Sprites/BiggestBoarder")
+    local scoreText = "Score: " .. text
+    local pressAText = "Press A To Restart"
+    local pressAImage = gfx.image.new(gfx.getTextSize(pressAText))
+    local gameOverImage = gfx.image.new(gfx.getTextSize(scoreText))
     gfx.pushContext(gameOverImage)
-        gfx.drawText(text, 0, 0)
+        gfx.drawText(scoreText, 0, 0)
     gfx.popContext()
+    gfx.pushContext(pressAImage)
+        gfx.drawText(pressAText, 0, 0)
+    gfx.popContext()
+    
+
+    local backgroundSprite = gfx.sprite.new(backgroundImage)
+    backgroundSprite:moveTo(200,120)
+    backgroundSprite:add()
+
+    local borderSprite = gfx.sprite.new(borderImage)
+    borderSprite:moveTo(200,100)
+    borderSprite:add()
+    
+    local borderSprite2 = gfx.sprite.new(borderImageButBigger)
+    borderSprite2:moveTo(200,140)
+    borderSprite2:add()
 
     local gameOverSprite = gfx.sprite.new(gameOverImage)
-    gameOverSprite:moveTo(200,120)
+    gameOverSprite:moveTo(200,100)
     gameOverSprite:add()
+    
+    local pressASprite = gfx.sprite.new(pressAImage)
+    pressASprite:moveTo(200,140)
+    pressASprite:add()
+    
 
     self:add()
 
